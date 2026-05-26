89f95914681565e802e9076122837a99c827ab84
ProtasMV <protasmv@gmail.com>
2026-05-26 21:55:10 +0300

    0.0.2 | Pupublic early access

--- src/main.rs
+++ src/main.rs
@@ -1,143 +1,165 @@
-use std::io;
-use rand::Rng;
+use std::io::{self, Write};
 use colored::*;
 
+const SYS_COLOR: Color = Color::TrueColor { r: 255, g: 255, b: 0 };
+const AUTOR_COLOR: Color = Color::TrueColor {r: 255, g: 165, b: 0};
+
 fn main() {
-    let mut user_mode = String::new();
-    let mut str_number_one = String::new();    
-    let mut str_number_two = String::new();
-
-    println!("{}", "Welcome to MyFirstCalculator!".blue());
-    println!("Mode: 1)Calculator, 2)match gym");
+    println!("{}", "Welcome to calculator".color(SYS_COLOR));
+    println!("{}", "Made by: @ProtasMV".color(AUTOR_COLOR));
     
-    io::stdin()
-        .read_line(&mut user_mode)
-        .expect("Calculator crash, eror code: 1");
+    println!();
+    println!("{}", "1)Calculator, 2)Math gym".color(SYS_COLOR));
+    let user_mode = input();
 
-    if user_mode.trim() == "1" {
-    
-    println!("Your first number?");
-
-    io::stdin()
-        .read_line(&mut str_number_one)
-        .expect("Calculator crash, eror code: 2");
-        
-    let number_one: f64 = str_number_one.trim().parse()
-    .expect("Calculator crash, eror code: 3");
-
-    println!("Your two number?");
-    
-    io::stdin() 
-        .read_line(&mut str_number_two)
-        .expect("Calculator crash, eror code: 4");
-   
-    let number_two: f64 = str_number_two.trim().parse()
-    .expect("Calculator crash, eror code: 5");
-
-    println!("Your action 1)+, 2)-, 3)*, 4)/ ");
-    let mut action = String::new();
-
-    io::stdin()
-        .read_line(&mut action)
-        .expect("Calculator crash, eror code: 6");
-
-    match action.trim() {
-        "1" => {let result = number_one + number_two;
-            println!("Result: {}", result)},
-        "2" => {let result = number_one - number_two;
-            println!("Result: {}", result)},
-        "3" => {let result = number_one * number_two;
-            println!("Result: {}", result)},
-        "4" => {let result = number_one / number_two;
-        println!("Result: {}", result)},
-        _ => println!("Calculator crash, eror code: 7"),
-    }
-
-    fn_press_to_end();  
-
-} 
-    
-    else if user_mode.trim() == "2" {
-        let random_number_one = rand::thread_rng().gen_range(0..=100);
-        let random_number_two = rand::thread_rng().gen_range(0..=100);
-        let mut str_user_anser_gym = String::new();
-        let rand_action = rand::thread_rng().gen_range(1..=3);
-        //let mut streak = 0;
-
-        let gym_answear = match rand_action {
-            1 => {
-                println!("{random_number_one} + {random_number_two}=?");
-                io::stdin()
-                .read_line(&mut str_user_anser_gym)
-                .expect("Calculator crash, eror code: 9"); 
-                random_number_one + random_number_two
-                },           
-           
-            2 => {
-                println!("{random_number_one} - {random_number_two}=?");
-                io::stdin()
-                .read_line(&mut str_user_anser_gym)
-                .expect("Calculator crash, eror code: 9");                
-                random_number_one - random_number_two
-                }, 
-
-            3 => {
-                println!("{random_number_one} * {random_number_two}=?");
+    match user_mode.trim().to_lowercase().as_str() {
+        "1"|"calculator"|"cal" => {
+            loop {
+                println!();
                 
-                io::stdin()
-                .read_line(&mut str_user_anser_gym)
-                .expect("Calculator crash, eror code: 9");              
-                random_number_one * random_number_two                
-                },
-            _ => {println!("Calculator crash, eror code: 13"); 0}
-            };
-            
-            let user_anser_gym: i32 = str_user_anser_gym.trim().parse().expect("Calculator crash, eror code: 8");
-            if user_anser_gym == gym_answear {
-                //streak = streak + 1;
-                println!("{}","Correct!".green());
-                //if streak>=2 {
-                //    if streak <= 4 {
-                //       println!("{}", "Streak: {streak}".yellow());
-                //    }
-                //    else if streak >= 5 {
-                //        println!("{}", "Streak: {streak}".red());
-                //    }
-                //    else {
-                //        println!("Calculator crash, eror code: 11");
-                //    } 
-                //}
-               
-                fn_press_to_end();       
-            } 
-            else {
-                println!("{}", "Wrong!".red());
-                //streak = 0;
-                //    println!("{}", "Streak: {streak}".red());
+                let user_first_num = input_number();
+                let user_two_num = input_number();
                 
-                fn_press_to_end();
-            
+                println!();
+                calc(user_first_num, user_two_num);
+
+                if user_want_continue() {continue}
+                else if !user_want_continue() {break}
             }
+        },
+        "2"|"math gym"|"gym" => {
+            loop {
+                println!();
+
+                let rand_num_1 = rand::random_range(1..5000);
+                let rand_num_2 = rand::random_range(1..5000);
+                
+                let rand_num_1 = rand_num_1 as f64;
+                let rand_num_2 = rand_num_2 as f64;
+
+                let rand_action = rand::random_range(1..3);
+                let answear_list = match rand_action {
+                    1 => (rand_num_1+rand_num_2, "+"),
+                    2 => (rand_num_1-rand_num_2, "-"),
+                    3 => (rand_num_1*rand_num_2, "*"),
+                    _ => {
+                        println!("Error 3"); (0.0, "?")
+                    }
+                };
+                
+                let (rand_answer, act) = answear_list;
+
+                println!("{rand_num_1}{act}{rand_num_2}=?");
+                loop {
+                    let user_try = loop {
+                        print!("Your guess: ");
+                        flush();
+
+                        let user_try = input();
+                        let user_try = parse_to_f64(&user_try.trim());
+                        let (user_try, error) = user_try;
+                        
+                        if error {continue}
+                        else if !error {break user_try}
+                    };
+
+                    println!();
+                    if user_try == rand_answer {
+                        println!("Corect!"); break;
+                    } else {
+                        println!("Incorect!")
+                    }
+                }
+
+                if user_want_continue() {continue}
+                else {break}
+            }
+        },
+        _ => {
             
+        }
     }
-    else {
-        println!("Calculator crash, eror code: 10");
+}
 
-        fn_press_to_end();
+fn calc(user_first_num: f64, user_two_num: f64) {
+    loop {
+        println!("{}", "Action: 1)+, 2)-, 3)*, 4)/".color(SYS_COLOR));
+        let user_action = input();
 
+       match user_action.trim().to_lowercase().as_str() {
+            "1"|"+" => {
+                println!("Result: {}", user_first_num+user_two_num)
+            },
+           "2"|"-" => {
+                println!("Result: {}", user_first_num-user_two_num)                    
+            },
+            "3"|"*" => {
+                println!("Result: {}", user_first_num*user_two_num)                    
+            },
+            "4"|"/" => {
+                if user_first_num == 0.0 || user_two_num == 0.0 {
+                    println!("{}", "Can't divide by 0")
+                } else {
+                    println!("Result: {}", user_first_num/user_two_num)                
+                }                    
+            },
+            _=> {
+                println!("{}", "Invalid input, try again");
+                continue;
+            }
+        }
     }    
 }
 
-fn fn_press_to_end() {   
-     let mut press_to_end = String::new();
-
-    println!("Press enter to close program");
-    io::stdin()
-        .read_line(&mut press_to_end)
-        .expect("Calculator crash, eror code: 12");     
+fn input_number() -> f64 {
+loop {
+        print!("{}", "Number: ".color(SYS_COLOR));
+        flush();
+        let user_first_num = input();
+        let user_first_num = parse_to_f64(&user_first_num);
+        let (user_first_num, error) = user_first_num;
+                        
+        if error {continue} else if !error {break user_first_num}
+    }    
 }
 
-//1.0 Base work calculator
-//1.1 rework if-else system to match and add gym mode
-//1.2 update gym mode (+, -, *) with use match and add colors for better UX
-//1.3 code optimization and add streak mehanick (dont work at this time but for future) to gym mode also fix bug when consol close)
+fn input() -> String {
+    let mut data = String::new();
+    io::stdin()
+        .read_line(&mut data)
+        .expect("Error 1");
+    data
+}
+
+fn flush() {
+    io::stdout()
+        .flush()
+        .expect("Error 2");
+}
+
+fn parse_to_f64(data: &str) -> (f64, bool) {
+    match data.trim().parse() {
+        Ok(num) => {(num, false)},
+        Err(er) => {
+            println!("Error: {er}, please try again");
+            (0.0, true)
+        }
+    }
+}
+
+fn user_want_continue() -> bool {
+    loop {
+        println!("{}", "Continue? 1)Yes, 2)No".color(SYS_COLOR));
+        let user_input = input();
+        
+        match user_input.trim().to_lowercase().as_str() {
+            "1"|"yes"|"y" => return true,
+            "2"|"no"|"n" => return false,
+            _ => {
+                println!("Invalid input, please try again");
+                println!();
+                continue
+            }
+        }
+    }
+}
